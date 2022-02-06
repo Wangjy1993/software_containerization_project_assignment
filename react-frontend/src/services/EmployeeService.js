425773ef942a8d8b7b4cee60f9da86302907b801
damon wang <wangjycode@gmail.com>
2022-02-06 18:17:32 +0100

    add frontend

--- react-frontend/src/services/EmployeeService.js
+++ react-frontend/src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URL = "https://34.110.167.12";
+const EMPLOYEE_API_BASE_URL = "https://34.110.167.12/api/v1/employees";
 
 class EmployeeService {
 
