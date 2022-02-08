9f277bc86bb6b281cb5fbd0c4d02e497c426e0e7
damon wang <wangjycode@gmail.com>
2022-02-08 10:09:47 +0100

    change service yaml

--- react-frontend/src/services/EmployeeService.js
+++ react-frontend/src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URL = "https://34.110.160.2/api/v1/employees";
+const EMPLOYEE_API_BASE_URL = "http://34.110.160.2/api/v1/employees";
 
 class EmployeeService {
 
